dd40f941e85b0520d5db7b67ab103515365092e1
Tomady <scanliberte@gmail.com>
2025-02-14 14:35:41 +0900

    feat: using JPA(CRUD)

--- springboot-developer/src/test/java/me/tomady/springbootdeveloper/MemberRepositoryTest.java
+++ springboot-developer/src/test/java/me/tomady/springbootdeveloper/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package me.tomady.springbootdeveloper;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -13,6 +14,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
+
+    @AfterEach
+    public void cleanUp() {
+        memberRepository.deleteAll();
+    }
 
     @Sql("/insert-members.sql")
     @Test
@@ -36,5 +42,49 @@ class MemberRepositoryTest {
         Member member = memberRepository.findByName("C").get();
 
         assertThat(member.getId()).isEqualTo(3);
+    }
+
+    @Test
+    void saveMember() {
+        Member member = new Member(1L, "A");
+
+        memberRepository.save(member);
+
+        assertThat(memberRepository.findById(1L).get().getName()).isEqualTo("A");
+    }
+
+    @Test
+    void saveMembers() {
+        List<Member> members = List.of(new Member(2L, "B"), new Member(3L, "C"));
+
+        memberRepository.saveAll(members);
+
+        assertThat(memberRepository.findAll().size()).isEqualTo(2);
+    }
+
+    @Sql("/insert-members.sql")
+    @Test
+    void deleteMemberById() {
+        memberRepository.deleteById(2L);
+
+        assertThat(memberRepository.findById(2L).isEmpty()).isTrue();
+    }
+
+    @Sql("/insert-members.sql")
+    @Test
+    void deleteAll() {
+        memberRepository.deleteAll();
+
+        assertThat(memberRepository.findAll().size()).isZero();
+    }
+
+    @Sql("/insert-members.sql")
+    @Test
+    void update() {
+        Member member = memberRepository.findById(2L).get();
+
+        member.changeName("BC");
+
+        assertThat(memberRepository.findById(2L).get().getName()).isEqualTo("BC");
     }
 }
