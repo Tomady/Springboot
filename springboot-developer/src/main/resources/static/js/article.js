d53bbbd23884d9b3ba5875949e956c9ead4692fc
Tomady <scanliberte@gmail.com>
2025-02-18 11:38:55 +0900

    feat: blog modify function

--- springboot-developer/src/main/resources/static/js/article.js
+++ springboot-developer/src/main/resources/static/js/article.js
@@ -1,3 +1,4 @@
+// delete btn
 const deleteButton = document.getElementById("delete-btn");
 
 if(deleteButton) {
@@ -11,5 +12,30 @@ if(deleteButton) {
             alert("삭제가 완료되었습니다.");
             location.replace("/articles");
         });
+    });
+}
+
+// modify btn
+const modifyButton = document.getElementById("modify-btn");
+
+if(modifyButton) {
+    modifyButton.addEventListener("click", event => {
+        let params = new URLSearchParams(location.search);
+        let id = params.get("id");
+
+        fetch(`/api/articles/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: document.getElementById("title").value,
+                content: document.getElementById("content").value
+            })
+        })
+        .then(() => {
+            alert("수정이 완료되었습니다.");
+            location.replace(`/articles/${id}`);
+        })
     });
 }
