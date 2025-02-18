f053985e92acd80d62cb9f11ec53823c990f9598
Tomady <scanliberte@gmail.com>
2025-02-18 10:27:07 +0900

    feat: FE blog delete function

---
+++ springboot-developer/src/main/java/me/tomady/springbootdeveloper/dto/ArticleListViewResponse.java
@@ -0,0 +1,17 @@
+package me.tomady.springbootdeveloper.dto;
+
+import lombok.Getter;
+import me.tomady.springbootdeveloper.domain.Article;
+
+@Getter
+public class ArticleListViewResponse {
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
