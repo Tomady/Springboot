c90063333e54f273d93d7150c718010dc180eee2
Tomady <scanliberte@gmail.com>
2025-02-25 15:47:23 +0900

    feat: login/out security

---
+++ springboot-developer/src/main/java/me/tomady/springbootdeveloper/service/UserDetailService.java
@@ -0,0 +1,19 @@
+package me.tomady.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.tomady.springbootdeveloper.domain.User;
+import me.tomady.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException(email));
+    }
+}
---
+++ springboot-developer/src/main/java/me/tomady/springbootdeveloper/service/UserService.java
@@ -0,0 +1,22 @@
+package me.tomady.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.tomady.springbootdeveloper.domain.User;
+import me.tomady.springbootdeveloper.dto.AddUserRequest;
+import me.tomady.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword())) // 패스워드 암호화
+                .build()).getId();
+    }
+}
